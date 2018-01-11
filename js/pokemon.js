77ba6bbc1e9dce75f85b8e15eb3f58a396d7ab45
Jon Lee <metrenome@gmail.com>
2018-01-10 23:12:02 -0500

    Finished up the basics of the PokeDex. Next step is to add some flair. Look up accordions.

--- js/pokemon.js
+++ js/pokemon.js
@@ -13,8 +13,10 @@ $(document).ready(function(){
           let results = data.results;
 
           $.each(results, function(key,value) {
-              console.log(value.name);
-              console.log(value.url);
+              // Debug logs
+              // console.log(value.name);
+              // console.log(value.url);
+              // console.log(key);
               $('.left-panel').append(" \
                 <div class='pokemon' data-id=" + (key + 1) + " + data-name=" + value.name + "> \
                           <a class='pokemon-url' href='#'> \
@@ -23,8 +25,45 @@ $(document).ready(function(){
                       </div>")
           });
           //Pokemon click handler
-          $("a.pokemon-url").click(function() {
-              alert("hi");
+          $('a.pokemon-url').click(function() {
+              // Start variables with a $ if it's a JQuery Object
+              let $pokeID = $(this).parent().attr('data-id');
+              let pokeURL = "https://pokeapi.co/api/v2/pokemon/" + $pokeID;
+
+              $.ajax({
+                dataType: 'json',
+                url: pokeURL,
+                type: "GET",
+                success: function(data) {
+                  console.log("success");
+                  $('.right-panel').empty();
+
+                  let pokeStats = data.stats;
+                  let pokeName = data.name;
+                   $('.right-panel').append(" \
+                        <div class='pokemon-name'> \
+                            " + pokeName + " \
+                        </div>");
+                  $.each(pokeStats, function(key, value) {
+                      let pokeStatName = value.stat.name;
+                      let pokeStatValue = value.base_stat;
+
+                      $('.right-panel').append(" \
+                        <div class='pokemon-stats'> \
+                            <div class='stat-name'> \
+                              " + pokeStatName + " \
+                            </div> \
+                            <div class='stat-value'> \
+                              " + pokeStatValue + " \
+                            </div> \
+                        </div>");
+                  });
+                },
+                error: function(data) {
+                  console.log("fail");
+                  console.log(data);
+                }
+              });
           })
         },
         error: function(data) {
